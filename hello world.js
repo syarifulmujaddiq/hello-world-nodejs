76bedefd0a37904090adbca2607e49e7e2e3d9b7
syarifulmujaddiq <86391272+syarifulmujaddiq@users.noreply.github.com>
2021-06-24 19:10:35 +0800

    Update hello world.js

--- hello world.js
+++ hello world.js
@@ -1,13 +1,23 @@
-const http = require('http');
+http = require("http");
 
-http.createServer((request, response) => {
+http.createServer(function (request, response) {
 
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-  
-    response.write('Hello, World!');
-     
-    response.end();
+// Send the HTTP header
 
-}).listen(4001); 
+// HTTP Status: 200 : OK
+
+// Content Type: text/plain
+
+response.writeHead(200, {'Content-Type': 'text/plain'});
+
+// Send the response body as "Hello World"
+
+response.end('Hello World');
+
+console.log(request.url)
+
+}).listen(3000);
+
+// Console will print the message
+
+console.log('Server running at http://129.213.54.196:4001/');
